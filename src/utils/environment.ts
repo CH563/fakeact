b73c02b3b8f5984f456b69e52032725beb0d0e5b
CH563 <wonderful563@qq.com>
2024-12-23 18:31:01 +0800

    add web

--- src/utils/environment.ts
+++ src/utils/environment.ts
@@ -28,6 +28,33 @@ export class EnvDetector {
     }
 }
 
+class BrowserOra {
+    // spinnerFrames = ['⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏'];
+    // frame = 0;
+    val: string;
+    constructor() {
+        this.val = ''
+    }
+    get text() {
+        return this.val;
+    }
+    set text(val: string) {
+        this.val = val;
+        if (EnvDetector.isBrowser() && (window as any).terminal) {
+            (window as any).terminal.write('\x1b[2K\r');
+            (window as any).terminal.write(`${val}`);
+            return;
+        }
+    }
+    start() {}
+    stop() {}
+    succeed() {}
+    fail() {}
+    warn() {}
+    info() {}
+    stopAndPersist() {}
+}
+
 export const writeLine = (str: string = '') => {
     if (EnvDetector.isBrowser() && (window as any).terminal) {
         (window as any).terminal.writeln(str);
@@ -36,9 +63,25 @@ export const writeLine = (str: string = '') => {
     console.log(str);
 }
 
+export const write = (str: string = '') => {
+    if (EnvDetector.isBrowser() && (window as any).terminal) {
+        (window as any).terminal.write(str);
+        return;
+    }
+    if (EnvDetector.isNode()) process.stdout.write(str);
+}
+
 // 获取终端宽度
 export function getTerminalWidth(): number {
     if (EnvDetector.isNode() && process.stdout.columns) return process.stdout.columns;
     if (EnvDetector.isBrowser() && (window as any).terminal.cols) return (window as any).terminal.cols;
     return 80;
+}
+
+export const oraFun = async () => {
+    if (EnvDetector.isNode()) {
+        const ora = (await import('ora')).default;
+        return ora();
+    }
+    return new BrowserOra();
 }
