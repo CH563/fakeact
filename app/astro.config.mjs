5a4384f6030c1c8cbdd341df81ef866c6b1f7914
CH563 <wonderful563@qq.com>
2026-02-03 12:51:54 +0800

    fix build in node22

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -2,7 +2,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import cloudflare from '@astrojs/cloudflare';
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
